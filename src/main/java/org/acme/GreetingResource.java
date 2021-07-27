fe53ddb557c8b338466889110e653478659d556c
Mumuksia <yurko.zavada@gmail.com>
2021-07-27 22:38:25 +0400

    init commit

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,33 @@
 package org.acme;
 
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/hello")
+@Path("/email")
 public class GreetingResource {
+
+    @Inject
+    Mailer mailer;
+
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+    @GET
+    @Path("/send")
+    public Response sendASimpleEmail(@PathParam("body") String body, @PathParam("to") String to) {
+        mailer.send(Mail.withText(to, "A simple email from quarkus", body));
+        return Response.accepted().build();
     }
 }
