b68ff213b81721e7c112df70f2ae0a40c2398897
Mumuksia <yurko.zavada@gmail.com>
2021-07-31 21:58:18 +0400

    sendinblue email

---
+++ src/main/java/org/acme/EmailResource.java
@@ -0,0 +1,70 @@
+package org.acme;
+
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+@Path("/email")
+@RequestScoped
+public class EmailResource {
+
+    @Inject
+    Mailer mailer;
+    @Inject
+    JsonWebToken jwt;
+
+
+    @GET
+    @Path("/hello")
+    @PermitAll
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(@Context SecurityContext ctx) {
+        return getResponseString(ctx);
+    }
+
+    @GET
+    @Path("/roles")
+    @RolesAllowed({ "User", "Admin" })
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloRolesAllowed(@Context SecurityContext ctx) {
+        return getResponseString(ctx) + ", birthdate: " + jwt.getClaim("birthdate").toString();
+    }
+
+
+    @GET
+    @Path("/send/{body}/{to}")
+    public Response sendASimpleEmail(@PathParam("body") String body, @PathParam("to") String to) {
+        mailer.send(Mail.withText(to, "A simple email from quarkus", body));
+        return Response.accepted().build();
+    }
+
+    private String getResponseString(SecurityContext ctx) {
+        String name;
+        if (ctx.getUserPrincipal() == null) {
+            name = "anonymous";
+        } else if (!ctx.getUserPrincipal().getName().equals(jwt.getName())) {
+            throw new InternalServerErrorException("Principal and JsonWebToken names do not match");
+        } else {
+            name = ctx.getUserPrincipal().getName();
+        }
+        return String.format("hello + %s,"
+                        + " isHttps: %s,"
+                        + " authScheme: %s,"
+                        + " hasJWT: %s",
+                name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJwt());
+    }
+
+    private boolean hasJwt() {
+        return jwt.getClaimNames() != null;
+    }
+}
--- src/native-test/java/org/acme/NativeEmailResourceIT.java
+++ src/native-test/java/org/acme/NativeEmailResourceIT.java
@@ -3,7 +3,7 @@ package org.acme;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+public class NativeEmailResourceIT extends EmailResourceTest {
 
     // Execute the same tests but in native mode.
 }
